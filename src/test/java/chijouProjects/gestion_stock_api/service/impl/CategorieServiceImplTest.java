1e67bcc507df235a0cc0ef5262ed763c0c6d4b01
dubois <duro.chijou@gmail.com>
2026-01-14 19:17:44 +0100

    2ème commit du 14-01-2026

---
+++ src/test/java/chijouProjects/gestion_stock_api/service/impl/CategorieServiceImplTest.java
@@ -0,0 +1,23 @@
+package chijouProjects.gestion_stock_api.service.impl;
+
+import chijouProjects.gestion_stock_api.service.CategorieService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class CategorieServiceImplTest {
+
+    @Autowired
+    private CategorieService service;
+
+    @Test
+    public void shouldSavedCategorieWithSuccess(){
+
+    }
+}
